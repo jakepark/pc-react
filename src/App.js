3946bdc37d44d19ad3219a53bfae5e682c3adef8
jake park <jake@parkjake.com>
2017-07-03 22:23:15 -0400

    before eject

--- src/App.js
+++ src/App.js
@@ -20,16 +20,16 @@ class App extends Component {
           
           <div className="links">
             <div className="pull-right">
-              <a className="mission" href="./mission" target="_blank">
+              <a className="mission" href="#mission" >
                 Mission
               </a>
-              <a className="dancers" href="./dancers" target="_blank">
+              <a className="dancers" href="#dancers" >
                 Dancers
               </a>
-              <a className="sponsors" href="./sponsors" target="_blank">
+              <a className="sponsors" href="#sponsors" >
                 Sponsors
               </a>
-              <a className="contact" href="./contact" target="_blank">
+              <a className="contact" href="#contact" >
                 Contact
               </a>
             </div>
@@ -38,7 +38,31 @@ class App extends Component {
         </nav>
       
         <main className="content">
+          
+          <h1 id="mission">Mission</h1>
+          
+          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+          <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
+          <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
+          <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
+          <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
+          <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
+          
+          <h1 id="dancers">Dancers</h1>
           <Dancers/>
+          
+          <h1 id="sponsors">Sponsors</h1>
+          
+          Special thanks to:
+          
+          Zenon Kay
+          
+          <h1 id="contact">Contact</h1>
+            Please email contact at contact.com
+          <h1>Footnote</h1>
+          <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
+          <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
+
           
         </main>
       </div>
--- src/Dancers.js
+++ src/Dancers.js
@@ -6,22 +6,30 @@ import './App.css';
 class Dancers extends Component {
   render() {
     const DANCERS_ARRAY = [
-      {name_first: "Alpha", name_last: "AbleLast", url: "url1", url_text:"url_text"},
-      {name_first: "Bravo", name_last: "BakerLast", url: "url2", url_text:"url_text"},
-      {name_first: "Charlie", name_last: "CharlieLast", url: "url3", url_text:"url_text"},
-      {name_first: "Delta", name_last: "DogLast", url: "url4", url_text:"url_text"},
-      {name_first: "Echo", name_last: "EasyLast", url: "url5", url_text:"url_text"},
-      {name_first: "Foxtrot", name_last: "FoxLast", url: "url5", url_text:"url_text"},
-      {name_first: "Golf", name_last: "GeorgeLast", url: "url5", url_text:"url_text"},
-      {name_first: "Hotel", name_last: "HowLast", url: "url5", url_text:"url_text"}
+      {name_first: "Alpha", name_last: "AbleLast", url: "url1", url_text:"url_text", img_url: "pic_1.jpeg"},
+      {name_first: "Bravo", name_last: "BakerLast", url: "url2", url_text:"url_text", img_url: "pic_2.jpeg"},
+      {name_first: "Charlie", name_last: "CharlieLast", url: "url3", url_text:"url_text", img_url: "pic_1.jpeg"},
+      {name_first: "Delta", name_last: "DogLast", url: "url4", url_text:"url_text", img_url: "pic_1.jpeg"},
+      {name_first: "Echo", name_last: "EasyLast", url: "url5", url_text:"url_text", img_url: "pic_2.jpeg"},
+      {name_first: "Foxtrot", name_last: "FoxLast", url: "url5", url_text:"url_text", img_url: "pic_2.jpeg"},
+      {name_first: "Golf", name_last: "GeorgeLast", url: "url5", url_text:"url_text", img_url: "pic_1.jpeg"},
+      {name_first: "Hotel", name_last: "HowLast", url: "url5", url_text:"url_text", img_url: "pic_2.jpeg"},
     ];
     
     return (
       <div id="dancers" className="dancers-list">
       
       {DANCERS_ARRAY.map(function(dancer, i) {
+        
+        var dancer_img = dancer.img_url;
+        var img_src = "/assets/" + dancer_img;
+        console.log(img_src);
+        
         return (
           <div className="dancer" key={i}>
+
+          // <img src={require('./assets/pic_1.jpeg')}></img>
+          <img src={require(img_src)}></img>
           <div>{dancer.name_first} {dancer.name_last}</div>
           <a href="{dancer.url}">{dancer.url_text}</a>
           </div>
