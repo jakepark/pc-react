0f9722c1a9533787cba143a87df61595282d7973
jake park <jake@parkjake.com>
2017-07-04 23:48:04 -0400

    image success

--- src/Dancers.js
+++ src/Dancers.js
@@ -39,8 +39,6 @@ class Dancers extends Component {
       {DANCERS_ARRAY.map(function(dancer, i) {
         return (
           <div className="dancer" key={i}>
-            <img className="dancer-portrait" src={require('./assets/pic_1.jpeg')}></img>
-          
             <a href="{dancer.url}">
               {this.createImage(dancer.img_url)}
             </a>
